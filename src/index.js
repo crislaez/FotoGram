de39727dd35b2e99fc69c9100412c6ab4e3f1ebb
CrisLaez <you@example.comcrislaez30@gmail.com>
2020-04-30 18:54:56 +0200

    cambio key

--- src/index.js
+++ src/index.js
@@ -4,13 +4,7 @@ import App from './Components/App/App'
 import firebase from 'firebase';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyA68UadJUznSAWeMBTr2WrlwXHXfjy29H0",
-  authDomain: "fotogram-55208.firebaseapp.com",
-  databaseURL: "https://fotogram-55208.firebaseio.com",
-  projectId: "fotogram-55208",
-  storageBucket: "fotogram-55208.appspot.com",
-  messagingSenderId: "364590719247",
-  appId: "1:364590719247:web:482013c740f9b0eaf3de7a"
+
 })
 
 ReactDOM.render(
